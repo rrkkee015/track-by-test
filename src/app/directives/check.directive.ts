fe4cea631dd0ad22166cea86efa7d4f19d60a067
dawn.o3o <dawn.o3o@kakaocorp.com>
2024-12-03 00:09:13 +0900

    feat: all options assemble

--- src/app/directives/check.directive.ts
+++ src/app/directives/check.directive.ts
@@ -14,10 +14,18 @@ export class CheckDirective {
     );
 
     observer.observe(element, {
+      // 자식 노드에 발생하는 변경사항 체크
+      childList: true,
       // 하위 모든 노드들의 변경사항 체크
       subtree: true,
+      // 해당 노드의 attribute 속성 변경사항 체크
+      attributes: true,
       // node.data에서 발생하는 변경사항 체크
       characterData: true,
+      // 이전 값도 콜백에 같이 포함하여 받는 기능, false시 변경된 값만 콜백에 전달
+      attributeOldValue: true,
+      // 이전 값도 콜백에 같이 포함하여 받는 기능, false시 변경된 값만 콜백에 전달
+      characterDataOldValue: true,
     });
   }
 }
