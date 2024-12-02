812f32744af577ca6f635fa23e12b3d0248afea1
dawn.o3o <dawn.o3o@kakaocorp.com>
2024-12-02 22:57:13 +0900

    feat: create test case using component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultTrackIdComponent } from './components/default/default-track-id.component';
+import { DefaultTrackObjComponent } from './components/default/default-track-obj.component';
+import { OnPushTrackIdComponent } from './components/onpush/on-push-track-id.component';
+import { OnPushTrackObjComponent } from './components/onpush/on-push-track-obj.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    DefaultTrackIdComponent,
+    DefaultTrackObjComponent,
+    OnPushTrackIdComponent,
+    OnPushTrackObjComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'track-by-test';
+  arr = [
+    { id: 1, name: 'a' },
+    { id: 2, name: 'b' },
+    { id: 3, name: 'c' },
+    { id: 4, name: 'd' },
+    { id: 5, name: 'e' },
+  ];
+
+  protected reset(): void {
+    this.arr = [
+      { id: 1, name: 'a' },
+      { id: 2, name: 'b' },
+      { id: 3, name: 'c' },
+      { id: 4, name: 'd' },
+      { id: 5, name: 'e' },
+    ];
+  }
 }
